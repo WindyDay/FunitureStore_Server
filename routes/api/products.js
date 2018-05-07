69fb298eba9a3e6aa8a81a297a0592fb6241d0fb
Nguyễn Đình Sơn <001.IceTea@gmail.com>
2018-05-08 00:50:41 +0700

    Paginate results of products http://localhost:3000/v1/api/products?page=2&maxResults=2

--- routes/api/products.js
+++ routes/api/products.js
@@ -5,7 +5,12 @@ const ProductsModel = require('../../models/products')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    var query = ProductsModel.find({});
+    let page = req.query.page ? req.query.page : 1;
+    let maxResults = req.query.maxResults ? req.query.maxResults : 16; //results per page
+    console.log('page: %s  - maxResults: %s', page, maxResults);
+    const query = ProductsModel.find({});
+    query.skip((page-1)*maxResults);
+    query.limit(1*maxResults);
     query.select('name oldPrice price thumbnail');
 
     query.exec((err, productsResult)=>{
@@ -14,7 +19,18 @@ router.get('/', function(req, res, next) {
             res.send(productsResult);
             
         }
+        else console.log(err);
     })
 });
-
+router.get('/:productId', function(req, res, next) {
+    var query = ProductsModel.findById(req.params.productId);
+    // console.log(req.params.productId);
+    query.exec((err, detailResult)=>{
+        if(!err){
+            // console.log(detailResult);
+            res.send(detailResult);
+            
+        }
+    })
+});
 module.exports = router;
