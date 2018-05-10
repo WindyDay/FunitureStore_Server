f0fcfb9e70973c99eaa96825d51bc92f4b83decb
Nguyễn Đình Sơn <001.IceTea@gmail.com>
2018-05-10 15:13:46 +0700

    fetch categories from api

--- routes/products.js
+++ routes/products.js
@@ -4,29 +4,53 @@ let axios = require('axios');
 const CONST = require('../constants');
 
 
-axios = axios.create({baseURL: CONST.ONL_API_PATH});
+_axios = axios.create({
+  baseURL: CONST.ONL_API_PATH
+});
 
 /* GET products page */
 router.get('/', function (req, res, next) {
 
-  axios.get('/products', {
+  function getProductsList() {
+    return _axios.get('/products', {
       params: req.query
     })
-    .then(function (response) {
-      res.render('products', {products: response.data});
-      console.log(response.data);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+  }
+
+  function getCategories() {
+    return _axios.get('/categories');
+
+  }
+  axios.all([getProductsList(), getCategories()])
+    .then(axios.spread(function (products, categories) {
+      console.log(categories.data);
+      res.render('products', {
+        products: products.data,
+        categories: categories.data,
+      });
+      // Both requests are now complete
+    }))
+    .catch(err => next(err));
+  // axios.get('/products', {
+  //     params: req.query
+  //   })
+  //   .then(function (response) {
+  //     res.render('products', {products: response.data});
+  //     console.log(response.data);
+  //   })
+  //   .catch(function (error) {
+  //     console.log(error);
+  //   });
 });
 
 router.get('/:productId', function (req, res, next) {
   let data = null;
 
-  axios.get('/products/'+req.params.productId)
+  axios.get('/products/' + req.params.productId)
     .then(function (response) {
-      res.render('product', {product: response.data});
+      res.render('product', {
+        product: response.data
+      });
       // console.log(response);
     })
     .catch(function (error) {
