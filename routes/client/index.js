822865fb82a9f231afcfbdd05dec918af867b2d7
Nguyễn Đình Sơn <001.IceTea@gmail.com>
2018-06-29 14:13:32 +0700

    disable functions relate to cookie

--- routes/client/index.js
+++ routes/client/index.js
@@ -7,10 +7,14 @@ const userRouter = require('./user')
 router.get('/', renderIndex);
 router.use('/products', productsRouter);
 router.use('/user', userRouter);
+router.get('/cart', renderCart);
 
 module.exports = router;
 
 
 function renderIndex(req,res,next){
     res.render('index')
+}
+function renderCart(req,res,next){
+    res.render('cart')
 }
